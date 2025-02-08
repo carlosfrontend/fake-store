672173bc7e3f8c083faf3ab9dcdab46b4c4404ec
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-02-08 03:00:55 +0100

    Fix: Solve merge problem

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,4 @@
 import { ShieldAlert } from 'lucide-react';
-<<<<<<< HEAD
 import styles from './ErrorPage.module.css';
 import { Link } from 'react-router-dom';
 
@@ -16,19 +15,3 @@ export const ErrorPage = () => {
     </section>
   );
 };
-=======
-import styles from './ErrorPage.module.css'
-import { Link } from 'react-router-dom';
-
-export const ErrorPage = () => {
-    return (
-        <section className={styles.errorPage}>
-           <div className={styles.circle}>
-               <ShieldAlert size={100} fill="none" color='#fff' />
-           </div>
-            <h1 className={styles.title}>Page not found 404</h1>
-            <Link className={styles.link} to="/">Back to home</Link>
-        </section>
-    )
-}
->>>>>>> routing
