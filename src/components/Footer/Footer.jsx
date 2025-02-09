fe3ae59df743372f90f1ca02ad464b80e39de424
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2025-02-09 02:29:01 +0100

    Feat: Styles for HomePage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,37 @@
-import styles from "./Footer.module.css"
+import styles from './Footer.module.css';
+import { SocialIcon } from 'react-social-icons';
 
 export const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-           <p>&copy; Fake Store</p>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.socialIcons}>
+        <SocialIcon
+          network='linkedin'
+          style={{ height: 25, width: 25 }}
+          url='https://www.linkedin.com/in/carlosfrontend/'
+          target='_blank'
+          fgColor='#336688'
+          bgColor='#fff'
+        />
+        <SocialIcon
+          network='github'
+          style={{ height: 25, width: 25 }}
+          target='_blank'
+          url='https://github.com/carlosfrontend'
+          fgColor='#336688'
+          bgColor='#fff'
+        />
+        <SocialIcon
+          network='x'
+          style={{ height: 25, width: 25 }}
+          url='https://x.com/CarlosFrontEnd'
+          fgColor='#336688'
+          bgColor='#fff'
+          target='_blank'
+        />
+      </div>
+
+      <small className={styles.copyright}>&copy; Fake Store - {new Date().getFullYear()}</small>
+    </footer>
+  );
+};
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom';
+import shoppingBags from '../../assets/shoppingBags.svg';
+import styles from './Home.module.css';
+
 export const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
+  return (
+    <section className={styles.home}>
+      <Link to='shop' title='Shop Now!'>
+        <img className={styles.img} src={shoppingBags} alt='shopping bags' />
+      </Link>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Welcome to Fake Store</h1>
+        <h2 className={styles.subtitle}>
+          Your one stop shop for all your shopping needs
+        </h2>
+        <div className={styles.linkContainer}>
+          <Link to='/shop' className={styles.link}>
+            Shop Now
+          </Link>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
